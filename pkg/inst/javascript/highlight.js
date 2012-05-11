b36a0d3f088e7d1d59d8a66961b2397772ecd60a
christoph (Christoph Molnar) <christoph.molnar@googlemail.com>
2012-05-11 14:22:59 +0000

    splitted brushing and highlighting
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/svgmaps@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/javascript/highlight.js
+++ pkg/inst/javascript/highlight.js
@@ -1,11 +1,13 @@
 
 $('circle[highlight]').mouseover(function() {
-	var value = $(this).attr('highlight');
-	// search all elements with this value
-	var bros = $('circle[highlight="' + value + '"]');
-	bros.attr('old', bros.attr('fill'));
-	bros.attr('fill', 'rgb(1,1,1)');
-    }
+    var value = $(this).attr('highlight');
+    // search all elements with this value
+    var bros = $('circle[highlight="' + value + '"]');
+    bros.attr('old', bros.attr('fill'));
+    bros.attr('fill', hcolour);
+    bros.attr('size_old', bros.attr('r'));
+    bros.attr('r', +hsize*bros.attr('r'));
+}
 );
 
 $('circle[highlight]').mouseout(function() {
@@ -13,16 +15,20 @@ $('circle[highlight]').mouseout(function() {
 	// search all elements with this value
 	var bros = $('circle[highlight="' + value + '"]');
 	bros.attr('fill', bros.attr('old'));
+    	bros.attr('r', bros.attr('size_old'));
     }
 );
 
 // path
 $('polyline[highlight]').mouseover(function() {
-	var value = $(this).attr('highlight');
-	// search all elements with this value
-	var bros = $('polyline[highlight="' + value + '"]');
-	bros.attr('old', bros.attr('stroke'));
-	bros.attr('stroke', 'rgb(1,1,1)');
+    var value = $(this).attr('highlight');
+    // search all elements with this value
+    var bros = $('polyline[highlight="' + value + '"]');
+    bros.attr('old', bros.attr('stroke'));
+    bros.attr('stroke', hcolour);
+    bros.attr('stroke-width_old', bros.attr('stroke-width'));
+    bros.attr('stroke-width', +hsize*bros.attr('stroke-width'));
+    add(+hsize*bros.attr('stroke-width'));
     }
 );
 
@@ -42,7 +48,7 @@ $('g[highlight]:has(polygon)').mouseover(function() {
 	//search all elements with this value
 	var bros = $('g[highlight="' + value + '"]:has("polygon")');
 	bros.children().attr('old', bros.children().attr('fill'));
-	bros.children().attr('fill','rgb(1,1,1)');
+	bros.children().attr('fill', hcolour);
     }
 );
 
@@ -55,60 +61,4 @@ $('g[highlight]:has(polygon)').mouseout(function() {
     }
 );
 
-
-// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// for brushing
-
-
-// function to find unique values
-Array.prototype.getUnique = function(){
-    var u = {}, a = [];
-    for(var i = 0, l = this.length; i < l; ++i){
-	if(this[i] in u)
-	    continue;
-	a.push(this[i]);
-	u[this[i]] = 1;
-    }
-    return a;
-};
-
-// all possible values for tag 'show'
-var vs = [];
-var show = $('[show]');
-show.each(function(index) {
-	vs[index] = $(this).attr("show");
-    });
-
-
-// value of the first shown objects
-curShow = "0";
-
-// make array of all possible values for show
-allShow = vs.getUnique();
-var allShow2 = [];
-for (var i = 0; i < allShow.length; i++){
-    allShow2[i] = +allShow[i];
-}
-allShow = allShow2.sort();
-
-
-$('[show]').attr('visibility', 'hidden');
-$('[show=' + curShow + ']').attr('visibility', 'visible');
-
-
-// when clicking on graphic make changes
-$('svg').click(function() {
-	// walk through array
-	var index = allShow.indexOf(curShow);
-	if ((index + 1) == allShow.length) {
-	    index = 0;
-	} else {
-	    index += 1
-		}
-	curShow = allShow[index];
-	// make all objects[show] invisible
-	$('[show]').attr('visibility', 'hidden');
-	// make all objects[show = curShow] visible
-	$('[show=' + curShow + ']').attr('visibility', 'visible');
-})
 
